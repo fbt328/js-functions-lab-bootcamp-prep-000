40987811089feef641c07f392d274484dd5974b8
Katherine Rust <katherine.rust.andrews@gmail.com>
2017-12-28 00:53:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays () {
+ return "Happy holidays!"; 
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${name};
+}
+
+function happyHolidayTo() {
+  return 'Happy Independence Day, you!';
+}
+
+function holidayCountdown(days, holiday) {
+  return 'It\'s ${days} until ${holiday}!'
+}
